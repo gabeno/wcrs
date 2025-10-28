d0948aa4c58dc315dbe51a29937ebb3c44f8e4bf
Gabe <gmajivu@gmail.com>
2025-10-28 22:38:01 +0300

    move tests into a module

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
-use std::io::{BufRead, Cursor};
+use std::io::BufRead;
 
 pub fn count_lines(input: impl BufRead) -> usize {
     input.lines().count()
 }
 
-#[test]
-fn count_lines_returns_number_of_lines_in_input() {
-    let input = Cursor::new("line 1\nline 2\n");
-    let lines = count_lines(input);
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
 
-    assert_eq!(lines, 2, "wrong line count");
+    use super::*;
+
+    #[test]
+    fn count_lines_returns_number_of_lines_in_input() {
+        let input = Cursor::new("line 1\nline 2\n");
+        let lines = count_lines(input);
+
+        assert_eq!(lines, 2, "wrong line count");
+    }
 }
