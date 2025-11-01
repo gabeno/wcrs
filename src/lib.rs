73f77f9d78890bcd2657359460129737d2ca08cb
Gabe <gmajivu@gmail.com>
2025-11-01 14:53:01 +0300

    feat: handle errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,41 @@
-use std::io::BufRead;
+use std::io::{BufRead, Result};
 
-pub fn count_lines(input: impl BufRead) -> usize {
-    input.lines().count()
+pub fn count_lines(input: impl BufRead) -> Result<usize> {
+    let mut count = 0;
+    for line in input.lines() {
+        line?;
+        count += 1;
+    }
+    Ok(count)
 }
 
 #[cfg(test)]
 mod tests {
-    use std::io::Cursor;
+    use std::io::{BufReader, Cursor, Error, Read, Result};
 
     use super::*;
+
+    struct ErrorReader;
+
+    impl Read for ErrorReader {
+        fn read(&mut self, _buf: &mut [u8]) -> Result<usize> {
+            Err(Error::other("oh no!"))
+        }
+    }
 
     #[test]
     fn count_lines_returns_number_of_lines_in_input() {
         let input = Cursor::new("line 1\nline 2\n");
-        let lines = count_lines(input);
+        let lines = count_lines(input).unwrap();
 
         assert_eq!(lines, 2, "wrong line count");
+    }
+
+    #[test]
+    fn count_lines_returns_error_if_present() {
+        let reader = BufReader::new(ErrorReader);
+        let result = count_lines(reader);
+
+        assert!(result.is_err(), "no error returned");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use std::io::stdin;
 use wcrs::count_lines;
 
 fn main() {
-    let lines = count_lines(stdin().lock());
+    let lines = count_lines(stdin().lock()).expect("found an error");
     println!("{lines} lines");
 }
