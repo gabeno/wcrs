cacf82dda8ea72c8367ca36e0cb89e8529f65bcd
Gabe <gmajivu@gmail.com>
2025-11-08 15:25:03 +0300

    feat: count lines for multiple files
    ```
    ➜  wcrs git:(main) ✗ cargo run src/*
       Compiling wcrs v0.1.0 (/Users/scr00ge/Workbench/wcrs)
        Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.11s
         Running `target/debug/wcrs src/lib.rs src/main.rs`
    46 lines
    14 lines
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::{env, fs::File, io::BufReader};
 
-use anyhow::{Context, Result};
+use anyhow::Result;
 use wcrs::count_lines;
 
 fn main() -> Result<()> {
-    let path = env::args().nth(1).context("Usage: count <FILE>")?;
-    let file = File::open(&path)?;
-    let file = BufReader::new(file);
-    let lines = count_lines(file)?;
-    println!("{lines} lines");
+    for path in env::args().skip(1) {
+        let file = File::open(&path)?;
+        let file = BufReader::new(file);
+        let lines = count_lines(file)?;
+        println!("{lines} lines");
+    }
     Ok(())
 }
