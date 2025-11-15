c7eb4f4ef43aefd2523f55374ff939d220b7980b
Gabe <gmajivu@gmail.com>
2025-11-15 15:21:33 +0300

    chore: add test for happy path

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,4 +1,5 @@
 use assert_cmd::{Command, cargo};
+
 use predicates::prelude::predicate;
 
 #[test]
@@ -7,4 +8,15 @@ fn binary_with_no_args_prints_usage() {
         .assert()
         .failure()
         .stderr(predicate::str::contains("Usage"));
+}
+
+#[test]
+fn binary_counts_lines_in_named_files() {
+    Command::new(cargo::cargo_bin!("wcrs"))
+        .args(&["tests/data/lines.txt", "tests/data/words.txt"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "tests/data/lines.txt: 2 lines\ntests/data/lines.txt: 2 words\ntests/data/words.txt: 2 lines\ntests/data/words.txt: 14 words",
+        ));
 }
