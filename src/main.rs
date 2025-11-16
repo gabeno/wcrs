d391dcae6202f04efb4bf1eaba723eb99a5fe702
Gabe <gmajivu@gmail.com>
2025-11-16 16:15:07 +0300

    feat: use clap

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
-use std::env;
+use clap::{Parser, arg};
 
-use anyhow::{Result, bail};
+use anyhow::Result;
 use wcrs::count_in_path;
 
+#[derive(Parser)]
+/// Count lines or words in the specified files
+struct Args {
+    /// Count words instead of lines
+    #[arg(short, long)]
+    words: bool,
+
+    /// Files to run count on
+    #[arg(required = true)]
+    files: Vec<String>,
+}
+
 fn main() -> Result<()> {
-    let args: Vec<_> = env::args().skip(1).collect();
-    let mut show_word_count = false;
-    if args.is_empty() {
-        bail!("Usage: wcrs [-w] <FILE>...");
-    }
-    for path in args {
-        if path == "-w" {
-            show_word_count = true;
-            continue;
-        }
+    let args = Args::parse();
+    for path in args.files {
         let counter = count_in_path(&path)?;
 
-        if show_word_count {
+        if args.words {
             println!("{path}: {} words", counter.words);
         } else {
             println!("{path}: {} lines", counter.lines);
